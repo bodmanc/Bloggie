f9da01e7414115b821e16cec1dd03b624972d29f
Cooper Bodman <cooperjack23@gmail.com>
2024-03-20 13:02:49 -0700

    Added initial migration to db

---
+++ Bloggie/Bloggie.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Data;
+
+namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+
+        public int Heading { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string UrlHandle { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public string Author { get; set; }
+
+        public bool Visible { get; set; }
+
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
